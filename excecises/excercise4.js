077c52875bf62c91edc81145049d436ef4c90452
Maria Castro <maria1.castro.stj@gmail.com>
2024-09-13 16:40:12 -0500

    Ejercicio 4

--- excecises/excercise4.js
+++ excecises/excercise4.js
@@ -6,7 +6,23 @@ Si la operación no es válida, debe retornar "Operación no reconocida".
 Recuerde que "No se puede dividir entre 0".
 */
 function calculadora(operacion, num1, num2) {
-    // TODO: Resolver
+    switch(operacion) {
+        case "+":
+        return num1 + num2
+        case "-":
+        return num1 - num2
+        case "*":
+        return num1 * num2
+        case "/":
+        if (num2 === 0){
+            return "No es posible dividir entre 0"
+        } else {
+            return num1/num2
+        }
+
+        default: 
+        return "Error en el operador"
+    }
 }
 
 console.log("-----------------------------------")
